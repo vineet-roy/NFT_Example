4c2299ee03c57b78e027fdc23da370027bc7521e
vineet-ids <vkumar@idssoft.com>
2024-05-18 16:29:00 +0530

    added contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,3 +1,21 @@
+// contracts/MyNFT.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721, Ownable {
+    uint256 public tokenCounter;
+
+    constructor() ERC721("MyNFT", "MNFT") {
+        tokenCounter = 0;
+    }
+
+    function createNFT(address to) public onlyOwner returns (uint256) {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(to, newTokenId);
+        tokenCounter += 1;
+        return newTokenId;
+    }
+}
